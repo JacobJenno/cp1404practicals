d78fbd63ba0d669d75fea1965592b3d2cb11ff3b
jacob j <jgjenno@outlook.com>
2026-06-04 14:20:55 +1000

    Refactor password check program to use functions

--- prac_02/prac_2.py
+++ prac_02/prac_2.py
@@ -1,10 +1,25 @@
 
+def main():
+    """Function docstring"""
+    # statements...
+
 min_length = 8
 
-password = input("Enter a password: ")
-
-while len(password) < min_length:
-    print("Password is too short.")
+def get_password():
+    global password
     password = input("Enter a password: ")
 
-print("*" * len(password))
+    while len(password) < min_length:
+        print("Password is too short.")
+        password = input("Enter a password: ")
+
+get_password()
+
+
+def password_hidden():
+    print("*" * len(password))
+
+password_hidden()
+
+main()
+
