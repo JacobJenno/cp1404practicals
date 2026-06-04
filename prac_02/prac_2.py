9d42aae961d2d24b7c557ec5b7f5e3b832249b0b
jacob j <jgjenno@outlook.com>
2026-06-04 16:48:38 +1000

    Prac 2

--- prac_02/prac_2.py
+++ prac_02/prac_2.py
@@ -1,14 +1,18 @@
 """"""""" PRAC 2 """""""""
+from random import random
+
 
 def main():
     choice = menu_display()
     menu_selection(choice)
 
 def menu_display():
+    print("")
     print("\033[4mPrac 2\033[0m")
     print("""
             1 - Password activity
             2 - Temperature activity
+            3 - Score activity
             Q - Quit (ends run)
                 """)
     choice = input(">>> ").upper()
@@ -21,6 +25,9 @@ def  menu_selection(choice):
             choice = menu_display()
         elif choice == "2":
             temperature_activity()
+            choice = menu_display()
+        elif choice == "3":
+            score_activity()
             choice = menu_display()
         else: break
 
@@ -101,6 +108,39 @@ def temperature_activity():
 
     user_choice = temperature_menu()
     temp_choice_maker(user_choice)
+""""""""""""""""""""""""""""""""""""""""""""""""
+""" SCORE ACTIVITY """
+def score_activity():
+    """Runs the password activity."""
+
+    def score_menu():
+        input_score = float(input("Enter score: "))
+        return input_score
+
+    def score_process(user_score):
+
+        if user_score < 0 or user_score > 100:
+            output = "Invalid score"
+        elif 0 <= user_score < 50:
+            output = "Bad"
+        elif 50 <= user_score < 90:
+            output = "Passable"
+        else:  # 90 to 100
+            output = "Excellent"
+
+        return output
+
+    def random_number():
+        import random
+        random_output = random.randint(1, 100)
+        return random_output
+
+    score = score_menu()
+    print("User score: ",score," = ", score_process(score))
+    if score_process(score) == "Excellent":
+        print("You get a prize!")
+    random_value = random_number()
+    print("Random score: ", random_value, " = ", score_process(random_value))
 
 """"""""""""""""""""""""""""""""""""""""""""""""
 main()
